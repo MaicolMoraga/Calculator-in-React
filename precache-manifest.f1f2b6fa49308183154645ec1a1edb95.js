e539af799a7be49b7f421e868abcc26af10b0645
Maicol Moraga <moraga.maicol@gmail.com>
2020-06-06 18:20:40 -0400

    Updates

---
+++ precache-manifest.f1f2b6fa49308183154645ec1a1edb95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "479f4482148961408919",
+    "url": "/calculator/static/css/main.749ece1b.chunk.css"
+  },
+  {
+    "revision": "479f4482148961408919",
+    "url": "/calculator/static/js/main.446377d6.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  },
+  {
+    "revision": "75f04d81fd3fcde38a92",
+    "url": "/calculator/static/css/2.772a1108.chunk.css"
+  },
+  {
+    "revision": "75f04d81fd3fcde38a92",
+    "url": "/calculator/static/js/2.eac0d26c.chunk.js"
+  },
+  {
+    "revision": "fa5ae8aa7c947ba2e760467daa1576f1",
+    "url": "/calculator/index.html"
+  }
+];
